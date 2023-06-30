3c02dd85131e07c2ddc3b8f37a98a0bc7507394f
Metin Yeni <metinyeni33@gmail.com>
2023-06-30 18:01:45 +0300

    status index sayfası oluşturuldu getall ve remove fonksiyonları yazıldı.

---
+++ CargoManagement.Web/Controllers/StatusController.cs
@@ -0,0 +1,33 @@
+﻿using CargoManagement.Repository.Shared.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CargoManagement.Web.Controllers
+{
+	public class StatusController : Controller
+	{
+		private readonly IUnitOfWork unitOfWork;
+
+		public StatusController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult GetAll()
+		{
+			return Json(new { data = unitOfWork.Statuses.GetAll() });
+		}
+
+		[HttpPost]
+		public IActionResult Remove(Guid id)
+		{
+			unitOfWork.Statuses.Remove(id);
+			unitOfWork.Save();
+			return Ok();
+		}
+	}
+}
