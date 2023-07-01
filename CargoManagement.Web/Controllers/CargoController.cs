3079b6a48b3cc6ad7fe84af51439f6755710c917
Metin Yeni <metinyeni33@gmail.com>
2023-07-01 14:25:07 +0300

    Teslimat rolüne kargo durumu değiştirme fonksiyonu eklendi.

--- CargoManagement.Web/Controllers/CargoController.cs
+++ CargoManagement.Web/Controllers/CargoController.cs
@@ -1,8 +1,10 @@
 ﻿using CargoManagement.Model;
 using CargoManagement.Repository.Shared.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
 
 namespace CargoManagement.Web.Controllers
 {
@@ -36,11 +38,27 @@ namespace CargoManagement.Web.Controllers
 			return Json(unitOfWork.Cargos.GetAll(x => x.Id == id).Include(c => c.Branch).Include(c => c.Status));
 		}
 
+		[Authorize]
 		public IActionResult Entry()
 		{
 			return View();
 		}
 
-		
+		[Authorize]
+		public IActionResult Status()
+		{
+			return View();
+		}
+
+		[Authorize(Roles ="Admin, Teslimat")]
+		[HttpPost]
+		public IActionResult ChangeStatus(Cargo cargoState)
+		{
+			Cargo asil = unitOfWork.Cargos.GetFirstOrDefault(x => x.Id == cargoState.Id);
+			asil.StatusId = cargoState.StatusId;
+			unitOfWork.Cargos.Update(asil);
+			unitOfWork.Save();
+			return Ok();
+		}
 	}
 }
