8b03f83da2fff3a6292c2076cb772cd58e818f8d
Metin Yeni <metinyeni33@gmail.com>
2023-07-04 23:43:36 +0300

    kullanıcı index sayfasında role adları görüntülendi. Kullanıcı ekleme ve düzenleme işlemleri tamamlandı. Silme işlmenindeki hata düzeltildi.

--- CargoManagement.Web/Controllers/UserController.cs
+++ CargoManagement.Web/Controllers/UserController.cs
@@ -4,90 +4,131 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
 using System.Security.Claims;
 
 namespace CargoManagement.Web.Controllers
 {
-    public class UserController : Controller
-    {
-        private readonly IUnitOfWork unitOfWork;
+	public class UserController : Controller
+	{
+		private readonly IUnitOfWork unitOfWork;
 
-        public UserController(IUnitOfWork unitOfWork)
-        {
-            this.unitOfWork = unitOfWork;
-        }
-        [Authorize(Roles ="Admin")]
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public UserController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+		}
+		[Authorize(Roles = "Admin")]
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult GetAll()
-        {
-            return Json(new { data = unitOfWork.Users.GetAll() });
-        }
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public IActionResult Create(User usr)
+		{
+			if (usr.Name != null)
+			{
+				unitOfWork.Users.Add(usr);
+				unitOfWork.Save();
+				return Ok();
+			}
+			else
+				return BadRequest();
+		}
 
-        [HttpPost]
-        public IActionResult Remove(Guid id)
-        {
-            unitOfWork.Users.Remove(id);
-            unitOfWork.Save();
-            return Ok();
-        }
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public IActionResult Update(User usr)
+		{
+			User asil = unitOfWork.Users.GetFirstOrDefault(x => x.Id == usr.Id);
+			if (asil.Name != null)
+			{
+				asil.Name = usr.Name;
+				asil.Password = usr.Password;
+				asil.UserName = usr.UserName;
+				asil.AuthorizationRoleId = usr.AuthorizationRoleId;
+				unitOfWork.Users.Update(asil);
+				unitOfWork.Save();
+				return Ok();
+			}
+			else
+			{
+				return BadRequest();
+			}
+		}
 
-        public IActionResult Login()
-        {
-            return View();
-        }
+		[HttpPost]
+		public IActionResult GetById(Guid id){
+			return Json(unitOfWork.Users.GetById(id));
+		}
 
-        public async Task<IActionResult> Logout(User user)
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "User");
-        }
+		public IActionResult GetAll()
+		{
+			return Json(new { data = unitOfWork.Users.GetAll().Include(x => x.AuthorizationRole) });
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Login(User user)
-        {
-            if (user != null)
-            {
-                User usr = unitOfWork.Users.GetFirstOrDefault(u => u.UserName == user.UserName && u.Password == user.Password && u.isActive == true && u.isDeleted == false);
-                if (usr != null)
-                {
-                    List<Claim> claims = new List<Claim>();
-                    claims.Add(new Claim(ClaimTypes.Name, usr.Name));
-                    claims.Add(new Claim(ClaimTypes.NameIdentifier, usr.Id.ToString()));
+		[HttpPost]
+		public IActionResult Remove(Guid id)
+		{
+			unitOfWork.Users.Remove(id);
+			unitOfWork.Save();
+			return Ok();
+		}
 
-                    if (usr.AuthorizationRoleId.ToString() == "1404b528-74f0-413a-963d-909315bbfd21")
-                    {
-                        claims.Add(new Claim(ClaimTypes.Role, "Admin"));
-                    }
-                    else if (usr.AuthorizationRoleId.ToString() == "dabe2585-1ca7-4224-9f36-d696283b6167")
-                    {
-                        claims.Add(new Claim(ClaimTypes.Role, "Teslimat"));
-                    }
-                    else if (usr.AuthorizationRoleId.ToString() == "32d82003-06a0-4b04-a585-c9ba20279c6a")
-                    {
-                        claims.Add(new Claim(ClaimTypes.Role, "Vezne"));
-                    }
+		public IActionResult Login()
+		{
+			return View();
+		}
 
-                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    return View();
-                }
-            }
+		public async Task<IActionResult> Logout(User user)
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Login", "User");
+		}
 
-            else
-            { 
-                return View(); 
-            }
-        }
+		[HttpPost]
+		public async Task<IActionResult> Login(User user)
+		{
+			if (user != null)
+			{
+				User usr = unitOfWork.Users.GetFirstOrDefault(u => u.UserName == user.UserName && u.Password == user.Password && u.isActive == true && u.isDeleted == false);
+				if (usr != null)
+				{
+					List<Claim> claims = new List<Claim>();
+					claims.Add(new Claim(ClaimTypes.Name, usr.Name));
+					claims.Add(new Claim(ClaimTypes.NameIdentifier, usr.Id.ToString()));
 
-    }
+					if (usr.AuthorizationRoleId.ToString() == "1404b528-74f0-413a-963d-909315bbfd21")
+					{
+						claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+					}
+					else if (usr.AuthorizationRoleId.ToString() == "dabe2585-1ca7-4224-9f36-d696283b6167")
+					{
+						claims.Add(new Claim(ClaimTypes.Role, "Teslimat"));
+					}
+					else if (usr.AuthorizationRoleId.ToString() == "32d82003-06a0-4b04-a585-c9ba20279c6a")
+					{
+						claims.Add(new Claim(ClaimTypes.Role, "Vezne"));
+					}
+
+					var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+					await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+					await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					return View();
+				}
+			}
+
+			else
+			{
+				return View();
+			}
+		}
+
+	}
 }
